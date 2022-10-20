e1f65aade550f3e040f1c47480b17c3be0aa3226
Mike Pattyn <mike.pattyn1@gmail.com>
2022-10-20 21:09:47 +0200

    seperate test for integration

---
+++ integration_test/app_test.dart
@@ -0,0 +1,34 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:iyltdsu_voice/main.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('end-to-end test', () {
+    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
+
+      expect(find.text('180'), findsOneWidget);
+
+      // // Verify that our counter starts at 0.
+      // expect(find.text('0'), findsOneWidget);
+      // expect(find.text('1'), findsNothing);
+
+      // // Tap the '+' icon and trigger a frame.
+      // await tester.tap(find.byIcon(Icons.add));
+      // await tester.pump();
+
+      // // Verify that our counter has incremented.
+      // expect(find.text('0'), findsNothing);
+      // expect(find.text('1'), findsOneWidget);
+    });
+  });
+}
