48e64258981a6c703693d985ef0b3b24ddd8a647
Mike <mike@methylium.com>
2023-07-09 13:20:45 +0200

    Working dartboard

--- lib/presentation/pages/dartboard_page.dart
+++ lib/presentation/pages/dartboard_page.dart
@@ -3,48 +3,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class DartboardPage extends StatefulWidget {
-  @override
-  _DartboardPageState createState() => new _DartboardPageState();
-}
-
-class _DartboardPageState extends State<DartboardPage> {
+class DartboardPage extends StatelessWidget {
   final GlobalKey webViewKey = GlobalKey();
 
   InAppWebViewController? webViewController;
   InAppWebViewOptions settings = InAppWebViewOptions();
 
-  PullToRefreshController? pullToRefreshController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    pullToRefreshController = kIsWeb ||
-            ![TargetPlatform.iOS, TargetPlatform.android]
-                .contains(defaultTargetPlatform)
-        ? null
-        : PullToRefreshController(
-            options: PullToRefreshOptions(
-              color: Colors.blue,
-            ),
-            onRefresh: () async {
-              if (defaultTargetPlatform == TargetPlatform.android) {
-                webViewController?.reload();
-              } else if (defaultTargetPlatform == TargetPlatform.iOS ||
-                  defaultTargetPlatform == TargetPlatform.macOS) {
-                webViewController?.loadUrl(
-                    urlRequest:
-                        URLRequest(url: await webViewController?.getUrl()));
-              }
-            },
-          );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  DartboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -56,8 +21,6 @@ class _DartboardPageState extends State<DartboardPage> {
               InAppWebView(
                 key: webViewKey,
                 initialFile: "assets/dartboard.html",
-                initialUserScripts: UnmodifiableListView<UserScript>([]),
-                pullToRefreshController: pullToRefreshController,
                 onWebViewCreated: (controller) async {
                   webViewController = controller;
                 },
